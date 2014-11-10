748de9f5cabc29458b4b5811a27598eab69240ac
juddam <juddmorgenstern@me.com>
2014-11-09 23:37:32 -0500

    firebase hooked up and printing data

--- FirstFire/ViewController.swift
+++ FirstFire/ViewController.swift
@@ -13,12 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create a reference to a Firebase location
+        var myRootRef = Firebase(url:"https://lawayla.firebaseio.com/vTesting/front-page/features")
+        
+        // Read data and react to changes
+        myRootRef.observeEventType(.Value, withBlock: {
+            snapshot in
+            println("\(snapshot.key) -> \(snapshot.value)")
+        })
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 }
 
