472727e77a6a3db88790963ebf9d6bd437356bc5
juddam <juddmorgenstern@me.com>
2014-11-09 19:06:07 -0500

    Initial Commit

---
+++ FirstFire/___VARIABLE_classPrefix:identifier___ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ___VARIABLE_classPrefix:identifier___ViewController.m
+//  FirstFire
+//
+//  Created by Mick Swagger on 11/9/14.
+//  Copyright (c) 2014 Mick Swagger. All rights reserved.
+//
+
+@interface ___VARIABLE_classPrefix:identifier___ViewController ()
+
+@end
+
+class implementation {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
